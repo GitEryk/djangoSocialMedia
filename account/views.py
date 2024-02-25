7a7b52724335d3fbc36b8fed92f431c21eec7371
Eryk Urban <urb2001an@gmail.com>
2024-02-25 17:35:52 +0100

    Add new themplates - login, logout, dashboard. Add new views

--- account/views.py
+++ account/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
+def dashboard(request):
+    return render(request, "account/dashbord.html", {"section": "dashboard"})
 
 def user_login(request):
     if request.method == "POST":
