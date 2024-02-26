03494b6d1e2d98521df735b4364c535f049542a3
Eryk Urban <urb2001an@gmail.com>
2024-02-26 19:42:42 +0100

    Added new urls - change password. Added new template - change password

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     # path("login/", views.user_login, name="login"),
+    # login logout urls
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # password modifi url
+    path("password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
+    path("password_change/done", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
     path("", views.dashboard, name="dashboard"),
 ]
