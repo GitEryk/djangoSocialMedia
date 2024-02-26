adb5da0f0f71b8c927f4b1accaa03cf2ed625525
Eryk Urban <urb2001an@gmail.com>
2024-02-26 20:39:28 +0100

    Added new urls - reset password. Added new templates - reset password

--- account/urls.py
+++ account/urls.py
@@ -7,8 +7,13 @@ urlpatterns = [
     # login logout urls
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # password modifi url
+    # password change url
     path("password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
-    path("password_change/done", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    path("password_change/done", auth_views.PasswordChangeDoneView.as_view(),
+         name="password_change_done"),
+    # password rest
+    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("password_reset/done", auth_views.PasswordResetDoneView.as_view(),
+         name="password_reset_done"),
     path("", views.dashboard, name="dashboard"),
 ]
