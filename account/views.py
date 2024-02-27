dec8dccf1e48c0143fdfaeaff95b4be19f38371b
Eryk Urban <urb2001an@gmail.com>
2024-02-27 20:23:04 +0100

    Added url path. Added new templates - register

--- account/views.py
+++ account/views.py
@@ -38,9 +38,9 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
-            return render(request, "account/registration_done.html",
+            return render(request, "account/register_done.html",
                           {"user_form": user_form})
     else:
         user_form = UserRegistrationForm()
-    return render(request, "account/registration_form.html",
+    return render(request, "account/register.html",
                   {"user_form": user_form})
