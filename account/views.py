fa7bfcaebf66ba6c306b93e1b882a98469184c06
Eryk Urban <urb2001an@gmail.com>
2024-02-27 19:08:39 +0100

    Added a new view - register

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -29,3 +29,18 @@ def user_login(request):
 
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data["password"])
+            new_user.save()
+            return render(request, "account/registration_done.html",
+                          {"user_form": user_form})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "account/registration_form.html",
+                  {"user_form": user_form})
