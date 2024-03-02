ff538a320541a64cc959ef98cd8c0f5c65c7f327
Eryk Urban <urb2001an@gmail.com>
2024-03-02 14:45:44 +0100

    Added a new method of using pipes to created a profile of a user who has registerd on socialmedia

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -127,10 +127,24 @@ AUTHENTICATION_BACKENDS = [
 SOCIAL_AUTH_FACEBOOK_KEY = 'xxx'
 SOCIAL_AUTH_FACEBOOK_SECRET = 'xxx'
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
-SOCIAL_AUTH_FACEBOOK_REDIRECT_URI = 'https://test-eryk.pl:8000/social-auth/complete/facebook'
+
+
 
 SOCIAL_AUTH_TWITTER_KEY = "xxx"
 SOCIAL_AUTH_TWITTER_SECRET = "xxx"
 
 SOCIAL_AUTH_GOOGLE_KEY = "xxx"
 SOCIAL_AUTH_GOOGLE_SECRET = "xxx"
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
